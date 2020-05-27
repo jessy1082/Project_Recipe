bd69bad256fabc63b1f941476577d0dbe8680c52
J C <jessy1082@yahoo.com>
2020-05-26 22:11:11 -0400

    v1 without js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+// var options = document.getElementById("listtext");
+// onclick = function () {
+//     if (listtext = ajiDeGallina
+// }
+
+// function ddlselect()
+// {
+//     var d = document.getElementById("ddselect");
+//     var displaytext= d.options(d.selectedIndex).text;
+//     document.getElementById("txtvalue").value=displaytext;
+// }
+
+function fn1()
+        {
+            var select = document.getElementById("ddselect");
+            console.log(select.options[select.selectedIndex].value)
+            //var selected = select.options[select.selectedIndex].value
+            var selected = select.selectedIndex
+            if(selected === 1) //redirect to Arroz Con Pato
+        }
+
+// const fn1 = subject => {
+//     console.log('subjec', subject);
+// }

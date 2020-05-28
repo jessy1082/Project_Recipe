92b82ce1d183a827e9dd263fe4ab6c8a8cdf4f9e
J C <jessy1082@yahoo.com>
2020-05-27 20:43:09 -0400

    videos

--- js/main.js
+++ js/main.js
@@ -15,12 +15,42 @@
 function fn1()
         {
             var select = document.getElementById("ddselect");
-            console.log(select.options[select.selectedIndex].value)
+            //console.log(select.options[select.selectedIndex].value)
             //var selected = select.options[select.selectedIndex].value
             var selected = select.selectedIndex
-            if(selected === 1) //redirect to Arroz Con Pato
-        }
+            if(selected === 1) { //redirect to Arroz Con Pato
+            window.document.location='./Ingredients.html';
+             } else if (selected === 2) {
+            window.document.location='./Ingredients2.html';
+             } else if (selected === 3) {
+            window.document.location='./Ingredients3.html';
+             } else {
+            console.log ('choose another')
+             }
+            }
 
-// const fn1 = subject => {
-//     console.log('subjec', subject);
-// }
+// Get the modal
+var modal = document.getElementById("myModal");
+    
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
